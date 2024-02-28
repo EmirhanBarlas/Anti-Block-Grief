921305df4f1b47fb529ec7f7f913c84edbf5147c
EmirhanBarlas <emirhanbarlas9@gmail.com>
2024-02-28 21:56:08 +0300

    Upload Files

--- src/main/java/org/splendid/antiblock/Antiblock.java
+++ src/main/java/org/splendid/antiblock/Antiblock.java
@@ -3,6 +3,9 @@ package org.splendid.antiblock;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
@@ -20,6 +23,19 @@ public final class Antiblock extends JavaPlugin {
         getLogger().info("Antiblock disabled!");
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("antiblockreload")) {
+            if (sender.hasPermission("antiblock.reload")) {
+                reloadConfig();
+                sender.sendMessage("Antiblock config dosyası yenilendi!");
+            } else {
+                sender.sendMessage("Bu komutu kullanma izniniz yok!");
+            }
+            return true;
+        }
+        return false;
+    }
     public List<String> getProtectedBlocks() {
         return getConfig().getStringList("protectedBlocks");
     }

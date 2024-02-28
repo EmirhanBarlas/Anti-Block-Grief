ba80aa421fec4a5dd7ae87b0bcb6801d95559f6d
EmirhanBarlas <emirhanbarlas9@gmail.com>
2024-02-28 21:34:16 +0300

    Upload Files

--- src/main/java/org/splendid/antiblock/Antiblock.java
+++ src/main/java/org/splendid/antiblock/Antiblock.java
@@ -1,17 +1,26 @@
 package org.splendid.antiblock;
 
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public final class Antiblock extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Antiblock enabled!");
-
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Antiblock disabled!");
+    }
+
+    public List<String> getProtectedBlocks() {
+        return getConfig().getStringList("protectedBlocks");
     }
 }
